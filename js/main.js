e1f8b67b0f577eeae219ceae2ed7a1424ed19232
unknown <sidicleiluiz@hotmail.com>
2020-10-21 22:05:57 -0300

    introdução ao javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Rafael Galleani ";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo " + nome + (n1*n2)); //Concatenando string com numeros
+console.log(nome);
+console.log(n1 * n2); //Fazendo operações com numeros
+//console.log(n1 * idade); //erro na operação devido a variavel idade não estar declarada
+console.log(frase.replace("Japão", "Brasil")); // replace Procura toda palavra Japão e muda para Brasil
+//console.log(frase.toLowerCase()); //toLowerCase Deixa tudo em letra minúscula
+console.log(frase.toUpperCase()); // Deixa tudo em letra maiúscula
